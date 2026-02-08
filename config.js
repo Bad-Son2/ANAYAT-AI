66e14fbf98a2ec901843dcd2bc7234bb0e813967
Bad-Son2 <thervilluckarlson6@gmail.com>
2026-02-08 11:23:54 -0500

    Update OWNER_NUMBER in config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923452401207",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50931772070",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝙰𝙽𝙰𝚈𝙰𝚃 𝙷𝙰𝙲𝙺𝙴𝚁",
 // add bot owner name
